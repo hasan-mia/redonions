61f79291384b1c8840060f0ae7e09c4ae9f2ad3b
hasanrafi69 <hasanrafi69@gmail.com>
2022-04-15 08:00:21 +0600

    route setup

--- src/Frontend/Banner/Banner.js
+++ src/Frontend/Banner/Banner.js
@@ -3,12 +3,13 @@ import './Banner.css'
 
 const Banner = () => {
 	return (
-		<section className='banner'>
+		<section className='banner text-center'>
 			<div className="container">
-				<div className='search'>
-					< form >
+				<div className='text-center'>
+					<h2 className='text-3xl font-semibold pt-36'>Best food waiting for bally</h2>
+					< form className='search'>
 						<input type="search" placeholder='search food items' className='py-3 px-40 w-100 rounded-3xl'/>
-						<button type='button' className='search-btn py-3 px-6 rounded-3xl text-white bg-red-500'> search </button>
+						<button type='button' className='search-btn py-3 px-10 rounded-3xl text-white bg-red-500'> search </button>
 					</form>
 				</div>
 			</div>
